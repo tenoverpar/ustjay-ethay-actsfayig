202f7a9839e5aa537c27b86acf30d9468361e3bc
Tracy A <tcpip201@gmail.com>
2018-07-31 21:04:21 -0700

    1st commit. Need to change the heroku path

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+#!/bin/env python
+# Internet Python
+# July 31, 2018
+
+
 import os
 
 import requests
@@ -19,10 +24,18 @@ def get_fact():
 
 @app.route('/')
 def home():
-    return "FILL ME!"
+    """Home function"""
+    fact = get_fact()
 
+    r = requests.post("http://hidden-journey-62459.herokuapp.com/piglatinize/",
+                      allow_redirects=False,
+                      data={'input_text': fact})
+
+    template = """
+    <a href={}>{}</a>
+    """
+    return template.format(r.headers['location'], r.headers['location'])
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
     app.run(host='0.0.0.0', port=port)
-
